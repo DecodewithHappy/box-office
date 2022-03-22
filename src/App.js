b876880c78ea8d4e4bd98aea97b30584d6aa7cf6
Happy <poddarsayan22@gmail.com>
2022-03-22 13:41:24 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
 
-        <Route>
-          <div>
-            This is 404 page
-          </div>
-        </Route>
-      </Switch>
+      <Route>
+        <div>
+          This is 404 page
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
